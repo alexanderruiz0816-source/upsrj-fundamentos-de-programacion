41dd04cf9eeace38f5caa7e2832a05059a6b4064
alexanderruiz0816-source <alexanderruiz0816@gmail.com>
2026-01-16 11:42:45 -0600

    Entrega 1: Area y Volumen

--- src/main.c
+++ src/main.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
-
 int main()
 {
-    int x = 4;
-    float y = 1.1;
-    int z = 50;
-    char letra_d = 'd';
+float lado, areaCuadrado,
+volumenCubo;
 
-    float suma = x + (int)y; 
+printf("introduce la longitud del lado: ");
+scanf("%f", &lado);
+areaCuadrado = lado*lado;
+
+//Operaciones del Cubo
+// ya mañana las pongo cawn
+volumenCubo = lado*lado*lado; 
+
+printf("\nResultados para el cuadrado:\n");
+printf("-Area: %.2f\n",areaCuadrado);
+
+printf("\nResultados para el Cubo:\n");
+printf("-Volumen: %.2f\n",volumenCubo);
 
 
-    printf("x: %i\ny: %f\nsuma: %f\n", x, y, suma);
-    return 0;
+
+return 0;
 }
+
+    
