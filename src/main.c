dd9f80cdbcac4e19a30e7f7b2daf85c87de592af
alexanderruiz0816-source <alexanderruiz0816@gmail.com>
2026-01-23 13:05:34 -0600

    Entrega 2 - par o impar

--- src/main.c
+++ src/main.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
+
+int is_even (int num){
+    if (num % 2 == 0 )
+    {
+      return 1;  
+    } else {
+        return 0;
+    }
+    
+}
+
 int main()
 {
-float lado, areaCuadrado,
-volumenCubo;
+int numero;
+printf("Ingrese Un Numero:");
+scanf("%d", &numero);
 
-printf("introduce la longitud del lado: ");
-scanf("%f", &lado);
-areaCuadrado = lado*lado;
-
-//Operaciones del Cubo
-// ya mañana las pongo cawn
-volumenCubo = lado*lado*lado; 
-
-printf("\nResultados para el cuadrado:\n");
-printf("-Area: %.2f\n",areaCuadrado);
-
-printf("\nResultados para el Cubo:\n");
-printf("-Volumen: %.2f\n",volumenCubo);
-
-
+if (is_even(numero))
+{
+    printf("El numero es par\n");
+} else {
+    printf("El numero es impar\n");
+}
 
 return 0;
 }
